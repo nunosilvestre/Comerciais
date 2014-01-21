71bc2af5cc218e998907162af43992155f6a6f69
nunosilvestre <nunosilvestre@gmail.com>
2014-01-21 00:49:55 +0000

    dois pontos

--- src/com/NAQS/comerciais/GoogleMaps.java
+++ src/com/NAQS/comerciais/GoogleMaps.java
@@ -53,44 +53,26 @@ import android.content.res.XmlResourceParser;
 public class GoogleMaps extends FragmentActivity 
 {
 	
-	private double longitute;
-	private double latitude;
+	private double longituteGPS = -9.1479472;
+	private double latitudeGPS = 38.7343564;
 	
 		    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.maps);
-	        //mLocationClient = new LocationClient(this, this, this);
-	        //Location mCurrentLocation = mLocationClient.getLastLocation();
 	        
-//	        mLocationClient = new LocationClient(this, this, this);
-//	        Location mCurrentLocation = mLocationClient.getLastLocation();
-//	        
-//	        mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
-//	        map = mapFragment.getMap();
-//
-//	        map.setMyLocationEnabled(true);
-	        
-	        
-//	        FragmentManager janelaFragmento = getSupportFragmentManager(); // Cria o objeto janelaFragmento do tipo FragmentManager e d‡-nos o gestor de fragmentos da nossa janela que Ž s— um
-//	        SupportMapFragment mapa=(SupportMapFragment) janelaFragmento.findFragmentById(R.id.map); // inicializa o nosso fragmento, com cast
-	        
-	       // GoogleMap map = mapa.getMap();
-	        
-	        //double latitudeAqui = getIntent().getDoubleExtra("latitudemapa", 0); //traz da activitymain o conteudo da variavel latitudemapa, com as coordenadas GPS
-	        //double longitudeAqui = getIntent().getDoubleExtra("longitudemapa",0);
-	        //JSONObject jsonObject = getLocationInfo("calcada galvao 139 porta b, 1400-167 lisboa");
-	        String partida = GoogleMaps.this.getIntent().getStringExtra(com.NAQS.comerciais.Inicio.GOOGLEMarkerIni);
 	        String chegada = GoogleMaps.this.getIntent().getStringExtra(com.NAQS.comerciais.Inicio.GOOGLEMarkerFim);
+	        //String[] moradas = {partida, chegada};
+	        
 	        new DownloadFilesTask().execute(chegada);
 	        
 
-	        double latitudeAqui = -33.867; //traz da activitymain o conteudo da variavel latitudemapa, com as coordenadas GPS
-	        double longitudeAqui = 151.206;
+	        //double latitudeAqui = 38.6775365; //traz da activitymain o conteudo da variavel latitudemapa, com as coordenadas GPS
+	        //double longitudeAqui = -9.17538643;
 //	        		double lat = mCurrentLocation.getAltitude();
 //	        		double lon = mCurrentLocation.getLongitude();
-	        		double lat = 38.6775365; //traz da activitymain o conteudo da variavel latitudemapa, com as coordenadas GPS
-	    	        double lon = -9.17538643;
+	        		//double lat = 38.6775365; //traz da activitymain o conteudo da variavel latitudemapa, com as coordenadas GPS
+	    	        //double lon = -9.17538643;
 //	        LatLng saldanha = new LatLng(latitude, longitute);
 //	        Toast.makeText (this,latitude+"",Toast.LENGTH_SHORT).show();
 //	        Toast.makeText (this,longitute+"",Toast.LENGTH_SHORT).show();
@@ -112,7 +94,9 @@ public class GoogleMaps extends FragmentActivity
 		        StringBuilder stringBuilder = new StringBuilder();
 		        try {
 		        //address = "calcada galvao 139 porta b, 1400-167 lisboa";
-		        address = address.replaceAll(" ","%20");    
+		        
+		        	address = address.replaceAll(" ","%20");
+		        	//address[1] = address[1].replaceAll(" ","%20");
 
 		        HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address + "&sensor=false");
 		        HttpClient client = new DefaultHttpClient();
@@ -167,20 +151,24 @@ public class GoogleMaps extends FragmentActivity
 			                .getJSONObject("geometry").getJSONObject("location")
 			                .getDouble("lat");
 
-			            LatLng saldanha = new LatLng(latitude, longitute);
+			            LatLng origem = new LatLng(latitudeGPS, longituteGPS);
+			            LatLng destino = new LatLng(latitude, longitute);
 //				        Toast.makeText (this,latitude+"",Toast.LENGTH_SHORT).show();
 //				        Toast.makeText (this,longitute+"",Toast.LENGTH_SHORT).show();
 			            FragmentManager janelaFragmento = getSupportFragmentManager(); // Cria o objeto janelaFragmento do tipo FragmentManager e d‡-nos o gestor de fragmentos da nossa janela que Ž s— um
 				        SupportMapFragment mapa=(SupportMapFragment) janelaFragmento.findFragmentById(R.id.map); // inicializa o nosso fragmento, com cast
 			            GoogleMap map = mapa.getMap();
 						map.addMarker(new MarkerOptions()
-				        .position(saldanha)
-				        .title("Hello world"));
+				        		.position(origem)
+				        			.title("origem"));
+						map.addMarker(new MarkerOptions()
+		        		.position(destino)
+		        			.title("destino"));
 				        map.setMyLocationEnabled(true);
 				        
 				        
 				        
-				       map.moveCamera(CameraUpdateFactory.newLatLngZoom(saldanha, 18)); //faz zoom ao ponto de 13 vezes
+				       map.moveCamera(CameraUpdateFactory.newLatLngZoom(destino, 18)); //faz zoom ao ponto de 13 vezes
 			        } catch (JSONException e) {
 			           
 
